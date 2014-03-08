d17a101511c5f5ebab031556f1ac17d371a452db
johniculusus <johndiaz.developer@gmail.com>
2014-03-09 02:44:51 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<title>
+		<?php
+		/*
+		 * Print the <title> tag based on what is being viewed.
+		 */
+		global $page, $paged;
+
+		wp_title( '|', true, 'right' );
+
+		// Add the blog name.
+		bloginfo( 'name' );
+
+		// Add the blog description for the home/front page.
+		$site_description = get_bloginfo( 'description', 'display' );
+		if ( $site_description && ( is_home() || is_front_page() ) )
+			echo " | $site_description";
+
+		// Add a page number if necessary:
+		if ( $paged >= 2 || $page >= 2 )
+			echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
+
+		?>
+	</title>
+	<?php
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+	?>
+</head>
+
+<body <?php body_class(); ?>>
+	HEADER
+	<!-- where other menus' like "Site Title", "Site Description", "Overview of Company", "Search Form" -->
+	<div id="page">
+		<!-- start of the content of your website what you are blogging -->
+		<div id="main">
---
+++ wp-content/themes/sampleJAFDtheme/content.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+THIS IS A EXAMPLE CONTENT
+<div>THIS IS A EXAMPLE CONTENT</div>
